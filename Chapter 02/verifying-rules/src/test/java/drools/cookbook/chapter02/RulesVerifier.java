641dd3fec2f37a4c4604605b770108be4bbf0040
Christian Katzorke <christian.katzorke@allianz.com>
2012-07-11 17:03:30 +0200

    fixed unit test, wrong assertion

--- Chapter 02/verifying-rules/src/test/java/drools/cookbook/chapter02/RulesVerifier.java
+++ Chapter 02/verifying-rules/src/test/java/drools/cookbook/chapter02/RulesVerifier.java
@@ -33,97 +33,106 @@ import org.junit.Test;
  */
 public class RulesVerifier {
 
-    private static Verifier verifier;
+	private static Verifier verifier;
 
-    @Test
-    public void rulesVerification() {
+	@Test
+	public void rulesVerification() {
 
-        boolean works = verifier.fireAnalysis();
+		boolean works = verifier.fireAnalysis();
 
-        assertTrue(works);
+		assertTrue(works);
 
-        VerifierReport result = verifier.getResult();
-        assertNotNull(result);
+		VerifierReport result = verifier.getResult();
+		assertNotNull(result);
 
-        assertEquals(3, result.getBySeverity(Severity.ERROR).size());
-        assertEquals(3, result.getBySeverity(Severity.WARNING).size());
-        assertEquals(0, result.getBySeverity(Severity.NOTE).size());
+		assertEquals(3, result.getBySeverity(Severity.ERROR).size());
+		assertEquals(2, result.getBySeverity(Severity.WARNING).size());
+		assertEquals(0, result.getBySeverity(Severity.NOTE).size());
 
-        Collection<VerifierMessageBase> noteMessages = result.getBySeverity(Severity.NOTE);
-        for (VerifierMessageBase msg : noteMessages) {
-            System.out.println("Note: " + msg.getMessage() + " type: " + msg.getMessageType() + " on: " + msg.getFaulty());
-        }
+		Collection<VerifierMessageBase> noteMessages = result
+				.getBySeverity(Severity.NOTE);
+		for (VerifierMessageBase msg : noteMessages) {
+			System.out.println("Note: " + msg.getMessage() + " type: "
+					+ msg.getMessageType() + " on: " + msg.getFaulty());
+		}
 
-        Collection<VerifierMessageBase> errorMessages = result.getBySeverity(Severity.ERROR);
-        for (VerifierMessageBase msg : errorMessages) {
-            System.out.println("Error: " + msg.getMessage() + " type: " + msg.getMessageType() + " on: " + msg.getFaulty());
-        }
+		Collection<VerifierMessageBase> errorMessages = result
+				.getBySeverity(Severity.ERROR);
+		for (VerifierMessageBase msg : errorMessages) {
+			System.out.println("Error: " + msg.getMessage() + " type: "
+					+ msg.getMessageType() + " on: " + msg.getFaulty());
+		}
 
-        Collection<VerifierMessageBase> warningMessages = result.getBySeverity(Severity.WARNING);
-        for (VerifierMessageBase msg : warningMessages) {
-            System.out.println("Warning: " + msg.getMessage() + " type: " + msg.getMessageType() + " on: " + msg.getFaulty());
-        }
+		Collection<VerifierMessageBase> warningMessages = result
+				.getBySeverity(Severity.WARNING);
+		for (VerifierMessageBase msg : warningMessages) {
+			System.out.println("Warning: " + msg.getMessage() + " type: "
+					+ msg.getMessageType() + " on: " + msg.getFaulty());
+		}
 
-        Collection<MissingRange> rangeCheckCauses = result.getRangeCheckCauses();
-        for (MissingRange missingRange : rangeCheckCauses) {
-            System.out.println(missingRange);
-        }
+		Collection<MissingRange> rangeCheckCauses = result
+				.getRangeCheckCauses();
+		for (MissingRange missingRange : rangeCheckCauses) {
+			System.out.println(missingRange);
+		}
 
-    }
+	}
 
-    @Test
-    public void generateVerifierReport() throws IOException {
+	@Test
+	public void generateVerifierReport() throws IOException {
 
-        boolean works = verifier.fireAnalysis();
+		boolean works = verifier.fireAnalysis();
 
-        assertTrue(works);
+		assertTrue(works);
 
-        VerifierReport result = verifier.getResult();
-        assertNotNull(result);
+		VerifierReport result = verifier.getResult();
+		assertNotNull(result);
 
-        assertEquals(3, result.getBySeverity(Severity.ERROR).size());
-        assertEquals(3, result.getBySeverity(Severity.WARNING).size());
-        assertEquals(0, result.getBySeverity(Severity.NOTE).size());
+		assertEquals(3, result.getBySeverity(Severity.ERROR).size());
+		assertEquals(2, result.getBySeverity(Severity.WARNING).size());
+		assertEquals(0, result.getBySeverity(Severity.NOTE).size());
 
-        VerifierReportWriter htmlWriter = VerifierReportWriterFactory.newHTMLReportWriter();
-        // VerifierReportWriter xmlWriter =
-        // VerifierReportWriterFactory.newXMLReportWriter();
-        // VerifierReportWriter txtWriter =
-        // VerifierReportWriterFactory.newPlainTextReportWriter();
+		VerifierReportWriter htmlWriter = VerifierReportWriterFactory
+				.newHTMLReportWriter();
+		// VerifierReportWriter xmlWriter =
+		// VerifierReportWriterFactory.newXMLReportWriter();
+		// VerifierReportWriter txtWriter =
+		// VerifierReportWriterFactory.newPlainTextReportWriter();
 
-        // Write to disk
-        FileOutputStream htmlOut = new FileOutputStream("report.zip");
-        // FileOutputStream xmlOut = new FileOutputStream("report.xml");
-        // FileOutputStream txtOut = new FileOutputStream("report.txt");
+		// Write to disk
+		FileOutputStream htmlOut = new FileOutputStream("report.zip");
+		// FileOutputStream xmlOut = new FileOutputStream("report.xml");
+		// FileOutputStream txtOut = new FileOutputStream("report.txt");
 
-        htmlWriter.writeReport(htmlOut, verifier.getResult());
-        // xmlWriter.writeReport(xmlOut, verifier.getResult());
-        // txtWriter.writeReport(txtOut, verifier.getResult());
+		htmlWriter.writeReport(htmlOut, verifier.getResult());
+		// xmlWriter.writeReport(xmlOut, verifier.getResult());
+		// txtWriter.writeReport(txtOut, verifier.getResult());
 
-        htmlOut.close();
-    }
+		htmlOut.close();
+	}
 
-    @BeforeClass
-    public static void beforeClass() {
-        VerifierBuilder vbuilder = VerifierBuilderFactory.newVerifierBuilder();
+	@BeforeClass
+	public static void beforeClass() {
+		VerifierBuilder vbuilder = VerifierBuilderFactory.newVerifierBuilder();
 
-        verifier = vbuilder.newVerifier();
+		verifier = vbuilder.newVerifier();
 
-        verifier.addResourcesToVerify(new ClassPathResource("rules.drl", RulesVerifier.class), ResourceType.DRL);
+		verifier.addResourcesToVerify(new ClassPathResource("rules.drl",
+				RulesVerifier.class), ResourceType.DRL);
 
-        if (verifier.hasErrors()) {
-            List<VerifierError> errors = verifier.getErrors();
-            for (VerifierError verifierError : errors) {
-                System.err.println(verifierError.getMessage());
-            }
-            fail("rules with errors");
-        }
+		if (verifier.hasErrors()) {
+			List<VerifierError> errors = verifier.getErrors();
+			for (VerifierError verifierError : errors) {
+				System.err.println(verifierError.getMessage());
+			}
+			fail("rules with errors");
+		}
 
-    }
+	}
 
-    @AfterClass
-    public static void afterClass() {
-        verifier.dispose();
-    }
+	@AfterClass
+	public static void afterClass() {
+		verifier.dispose();
+	}
 
 }
